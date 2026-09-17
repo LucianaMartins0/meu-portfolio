dccb66db5684edf4bce047632b88f93dd3a8c2dd
LucianaMartins0 <martins.luciana1911@gmail.com>
2026-09-16 22:59:31 -0300

    Trocando mailto pelo Formspree no formulário de contato

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Função que recebe o número do botão clicado (0, 1, 2, 3 ou 4)
         function mudarTab(index) {
             
-            // PASSO 1: O APAGÃO (Esconder tudo)
+            // PASSO 1: Esconder tudo
             // Pega todos os textos de conteúdo
             const todosConteudos = document.querySelectorAll('[id^="content-"]');
             // Pega todos os botões do menu
@@ -17,7 +17,7 @@
                 li.classList.add('border-transparent', 'text-[#6D3914]');
             });
 
-            // PASSO 2: A REVELAÇÃO (Mostrar só o escolhido)
+            // PASSO 2: Mostrar só o escolhido
             
             // Pega o texto que tem o número que você clicou
             const conteudoParaMostrar = document.getElementById(`content-${index}`);
